1591cdfd336445e71b67d011fee951f714349b09
fc0n <jeangabrieljb9@gmail.com>
2022-06-20 15:29:39 -0300

    atualizando texto

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -8,10 +8,10 @@ function NavBar() {
       <nav className="container">
         <div className="dsmovieNavContent">
           <h1>DSMovie</h1>
-          <a href="https://github.com/devsuperior">
+          <a href="https://github.com/fc0n">
             <div className="dsmovieContactContainer">
               <GithubIcon />
-              <p className="dsmovieContactLink">/devsuperior</p>
+              <p className="dsmovieContactLink">/fc0n</p>
             </div>
           </a>
         </div>
